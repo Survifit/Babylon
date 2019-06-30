9311687c1b09a2a8b5734f68edcc7134d48fd9ff
Purple Instance <47543437+snhartje@users.noreply.github.com>
2019-06-30 18:30:59 -0500

    adding comments to explain how the globe javascript works

--- tmp/TestGlobe - Copy (2) - Copy/static/js/graph2.js
+++ tmp/TestGlobe - Copy (2) - Copy/static/js/graph2.js
@@ -21,16 +21,16 @@ enableRotation();
 function drawGlobe() {  
     d3.queue()
         .defer(d3.json, 'https://gist.githubusercontent.com/mbostock/4090846/raw/d534aba169207548a8a3d670c9c2cc719ff05c47/world-110m.json')          
-        .defer(d3.json, 'language_status_UNESCO.json')
-        .await((error, worldData, locationData) => {
+        .defer(d3.json, 'static/js/locations.json')
+        .await((error, worldData, locationData) => { //names the two data sources - i think....
             svg.selectAll(".segment")
                 .data(topojson.feature(worldData, worldData.objects.countries).features)
                 .enter().append("path")
                 .attr("class", "segment")
                 .attr("d", path)
-                .style("stroke", "#888")
+                .style("stroke", "#888")//outline of countries
                 .style("stroke-width", "1px")
-                .style("fill", (d, i) => '#e5e5e5') //set up an if statement or switch statement
+                .style("fill", (d, i) => '#e5e5e5') //fill color for countries
                 .style("opacity", ".6");
                 locations = locationData;
                 drawMarkers();                   
@@ -45,8 +45,8 @@ function drawGraticule() {
         .datum(graticule)
         .attr("class", "graticule")
         .attr("d", path)
-        .style("fill", "#fff")
-        .style("stroke", "#ccc");
+        .style("fill", "#fff") //background of globe - think this needs to be same color as overall background
+        .style("stroke", "#ccc"); //latitude and longitude lines
 }
 
 function enableRotation() {
@@ -65,14 +65,14 @@ function drawMarkers() {
         .enter()
         .append('circle')
         .merge(markers)
-        .attr('cx', d => projection([d.longitude, d.latitude])[0])
-        .attr('cy', d => projection([d.longitude, d.latitude])[1])
+        .attr('cx', d => projection([d.longitude, d.latitude])[0]) //converts latitude and longitude to x and y values
+        .attr('cy', d => projection([d.longitude, d.latitude])[1]) //seems to require having the pair and then choosing which value in the array rather than just d.lon for cx and d.lat for cy
         .attr('fill', d => {
             const coordinate = [d.longitude, d.latitude];
-            gdistance = d3.geoDistance(coordinate, projection.invert(center));
-            return gdistance > 1.57 ? 'none' : 'steelblue';
+            gdistance = d3.geoDistance(coordinate, projection.invert(center)); //projection.invert converts x and y back to lat and lon
+            return gdistance > 1.57 ? 'none' : 'steelblue'; //color of dots
         })
-        .attr('r', 7);
+        .attr('r', 7); //size of the dot
 
     markerGroup.each(function () {
         this.parentNode.appendChild(this);
