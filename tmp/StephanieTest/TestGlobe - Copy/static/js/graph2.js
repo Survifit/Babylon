d5b5364db67a9541251cf3718adebec6bfed13fd
Devin Stern <writedevin@gmail.com>
2019-07-04 11:03:22 -0500

    blurb added

--- tmp/StephanieTest/TestGlobe - Copy/static/js/graph2.js
+++ tmp/StephanieTest/TestGlobe - Copy/static/js/graph2.js
@@ -3,6 +3,6 @@ var container = document.getElementById( "globalArea" );
 // Create Gio.controller
 var controller = new GIO.Controller( container );
 // Add data
-controller.addData( data );
+controller.addData(data);
 // Initialize and render the globe
 controller.init();
